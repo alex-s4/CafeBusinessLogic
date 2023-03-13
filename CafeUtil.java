b49c54b89890ceb873525e0bd0cd0d58602ad926
alex-s4 <al39726867@gmail.com>
2023-03-13 17:02:09 +0800

    Implement the addCustomer method and test

--- CafeUtil.java
+++ CafeUtil.java
@@ -49,7 +49,15 @@ public class CafeUtil{
         }
     }
 
-    
+
+    public void addCustomer(ArrayList<String> customers){
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.printf("Hello, %s", userName + "\n");
+        System.out.println("There are " +customers.size()+ " people in front of you");
+        customers.add(userName);
+        System.out.println(customers);
+    }
 
 
 
